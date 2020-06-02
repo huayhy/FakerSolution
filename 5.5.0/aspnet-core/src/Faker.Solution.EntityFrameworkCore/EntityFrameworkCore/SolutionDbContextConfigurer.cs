32842e94d6b47470daf1ec216c2656b411c2aac2
huayhy <huayhy@126.com>
2020-06-02 15:49:58 +0800

    设置了默认的链接字符串

--- 5.5.0/aspnet-core/src/Faker.Solution.EntityFrameworkCore/EntityFrameworkCore/SolutionDbContextConfigurer.cs
+++ 5.5.0/aspnet-core/src/Faker.Solution.EntityFrameworkCore/EntityFrameworkCore/SolutionDbContextConfigurer.cs
@@ -8,11 +8,13 @@ namespace Faker.Solution.EntityFrameworkCore
         public static void Configure(DbContextOptionsBuilder<SolutionDbContext> builder, string connectionString)
         {
             builder.UseSqlServer(connectionString);
+            //builder.UseMySql(connectionString);
         }
 
         public static void Configure(DbContextOptionsBuilder<SolutionDbContext> builder, DbConnection connection)
         {
             builder.UseSqlServer(connection);
+            //builder.UseMySql(connection);
         }
     }
 }
